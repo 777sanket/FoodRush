4cc620852b8f560dbfedf682ca2b647fec8b7cb3
777sanket <sanketwasnik1902@gmail.com>
2023-07-31 16:45:50 +0530

    day2_1part

---
+++ frontend/src/components/Search/Search.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import classes from './search.module.css';
+import { useNavigate, useParams } from 'react-router-dom';
+
+export default function Search() {
+  const [term, setTerm] = useState('');
+  const navigate = useNavigate();
+  const { searchItem } = useParams();
+
+  
+  const search = async () => {
+    term ? navigate('/search/' + term) : navigate('/');
+  };
+
+  return (
+    <div className={classes.container}>
+      <input
+        type="text"
+        placeholder="Search FoodRush!"
+        onChange={e => setTerm(e.target.value)}
+        onKeyUp={e => e.key === 'Enter' && search()}
+        defaultValue={searchItem}
+      />
+      <button onClick={search}>Search</button>
+    </div>
+  );
+}
