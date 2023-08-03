3745b8da6c4f5f87ea08d64ba924e92d1c384166
777sanket <sanketwasnik1902@gmail.com>
2023-08-03 18:12:08 +0530

    Day4_LoginPage

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,19 @@
 import express from 'express';
 import cors from 'cors';
 import foodRouter from './routers/food.router.js';
-
+import userRouter from './routers/user.router.js';
 const app = express();
+app.use(express.json());
 
 app.use(
   cors({
     credentials: true,
-    origin: ['http://localhost:3001'],
+    origin: ['http://localhost:3002'],
   })
 );
 
 app.use('/api/foods', foodRouter);
-
-
+app.use('/api/users', userRouter);
 
 const PORT = 5001;
 app.listen(PORT, () => {
