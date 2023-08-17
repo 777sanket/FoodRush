eb6d3dc2a9094a6ae8f535ffe7bef0943554fa54
777sanket <sanketwasnik1902@gmail.com>
2023-08-17 14:00:02 +0530

    PaymentPage_Fix1

--- frontend/src/AppRoutes.js
+++ frontend/src/AppRoutes.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import HomePage from './pages/Home/HomePage';
-import FoodPage from './pages/Food/FoodPage';
 import CartPage from './pages/Cart/CartPage';
+import FoodPage from './pages/Food/FoodPage';
+import HomePage from './pages/Home/HomePage';
 import LoginPage from './pages/Login/LoginPage';
 import RegisterPage from './pages/Register/RegisterPage';
 import AuthRoute from './components/AuthRoute/AuthRoute';
@@ -12,29 +12,29 @@ import PaymentPage from './pages/Payment/PaymentPage';
 export default function AppRoutes() {
   return (
     <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/search/:searchTerm" element={<HomePage />} />
-        <Route path="/tag/:tag" element={<HomePage />} />
-        <Route path="/food/:id" element={<FoodPage />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route 
-          path="/checkout" 
-          element={
-            <AuthRoute>
-              <CheckoutPage />
-            </AuthRoute>
-          } 
-        />
-        <Route 
-          path="/payment" 
-          element={
-            <AuthRoute>
-              <PaymentPage />
-            </AuthRoute>
-          } 
-        />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/search/:searchTerm" element={<HomePage />} />
+      <Route path="/tag/:tag" element={<HomePage />} />
+      <Route path="/food/:id" element={<FoodPage />} />
+      <Route path="/cart" element={<CartPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route
+        path="/checkout"
+        element={
+          <AuthRoute>
+            <CheckoutPage />
+          </AuthRoute>
+        }
+      />
+      <Route
+        path="/payment"
+        element={
+          <AuthRoute>
+            <PaymentPage />
+          </AuthRoute>
+        }
+      />
     </Routes>
   );
 }
